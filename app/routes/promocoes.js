8f170fe875031beb0aeeebf0d48e07f45e6f3df3
Michel Medeiros <michel.tds@gmail.com>
2017-02-02 21:36:00 -0200

    Socket Middleare

--- app/routes/promocoes.js
+++ app/routes/promocoes.js
@@ -1,44 +1,19 @@
 var Livros = require('../models/livro');
 
 module.exports = function(app) {
-    app.get("/api/promocoes", function(req, res) {
+    app.get("/api/promocoes/form", function(req, res, next) {
         Livros.find(function(err, livros) {
             if (err) {
                 return next(err);
             }
             res.render('promocoes/form', {
                 livros: livros
-                    // errosValidacao: {}
             });
         });
     });
-
-    // app.get('/api/promocoes/form', function(req, res) {
-    //     res.render('promocoes/form', {
-    //         errosValidacao: {},
-    //         livros: {}
-    //     });
-    // });
-
-    app.post('/api/promocoes', function(req, res) {
+    app.post("/api/promocoes", function(req, res) {
         var promocao = req.body;
-        req.assert('mensagem', 'Mensagem é obrigatória').notEmpty();
-        var erros = req.validationErrors();
-        var livros = [];
-        if (erros) {
-            console.log('Erros encontrados no cadastro: ' + erros[0].param + ' - ' + erros[0].msg);
-            // res.redirect('/api/promocoes');
-            // return res.status(400).render('promocoes/form', {
-            //     errosValidacao: erros,
-            //     promocao: promocao,
-            //     livros: []
-            // });
-        }
-        // Promocoes.create(promocao, function(err, livros) {
-        //     if (err) res.send(err);
-        // console.log('INSERT nova promocao: ' + promocao.mensagem);
-        // return res.redirect('/api/promocoes/form');
-        // });
+        app.get('io').emit('novaPromocao', promocao);
+        res.redirect("/api");
     });
-
 }
